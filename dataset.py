2e75fda16a19134e05bfdd39f387ac1a8685b233
fabio <s276842@studenti.polito.it>
2021-12-23 11:39:37 +0100

    refactoring dataset

--- dataset.py
+++ dataset.py
@@ -1,68 +1,60 @@
 import json
-
 from torch.utils.data import Dataset
 
+SPECIAL_TOKENS = {
+    'U' : '<U>',    # Token for beginning of User utterance
+    'S' : '<S>',    # Token for beginning of System utterance
+    'Ka' : '<Ka>',  # Token for beginning of Answer
+    'Kq' : '<Kq>'   # Token for beginning of Question
+}
+
+#todo implement truncation of context (given number of utternaces)
+#todo implement choice for special tokens to use
+#todo implement alignement of knowledge to ground-truth
 class MultiWOZDataset(Dataset):
-    def __init__(self, path_to_logs, path_to_labels, path_to_knowledge, max_length=40):
 
-        # self.tokenizer = tokenizer
-        self.special_tokens = {'U' : '<U>',
-                               'S' : '<S>',
-                               'Ka' : '<Ka>'
-                               }
-
-        knowledge_base = json.load(open(path_to_knowledge))
+    def __init__(self, path_to_logs, path_to_labels, path_to_knowledge):
+        self.knowledge_base = json.load(open(path_to_knowledge))
         labels = json.load(open(path_to_labels))
         logs = json.load(open(path_to_logs))
 
-        self.context_list = []
-        self.knowledge_doc_list = []
-        self.response_list = []
+        knowledge_seeking_turns = [i for i, label in enumerate(labels) if label['target'] is True]
+        self.labels = [labels[i] for i in knowledge_seeking_turns]
+        self.logs = [logs[i] for i in knowledge_seeking_turns]
 
-        # move to get_item?
-        for label, log in zip(labels, logs):
-            if label['target'] == True:
-
-                # retrieve info of the knowledge snippet
-                knowledge_snippet = label['knowledge'][0]
-                domain = knowledge_snippet['domain']
-                entity_id = str(knowledge_snippet['entity_id'])
-                doc_id = str(knowledge_snippet['doc_id'])
-                document = knowledge_base[domain][entity_id]['docs'][doc_id]
-
-                # retrieve ground-truth response
-                response = label['response'].strip()
-
-                self.context_list.append(log)
-                self.knowledge_doc_list.append(document)
-                self.response_list.append(response)
-
-        del knowledge_base, labels, logs
+        del logs, labels, knowledge_seeking_turns
 
     def __len__(self):
-        return len(self.context_list)
+        return len(self.labels)
 
     def __getitem__(self, item):
-        dialog_context = self.context_list[item]
-        knowledge_document = self.knowledge_doc_list[item]
-        question = knowledge_document['title'].strip()
-        answer = knowledge_document['body'].strip()
+        label = self.labels[item]
+        dialog_context = self.logs[item]
 
-        target = self.response_list[item]
+        # Retrieve knowledge document
+        knowledge_snippet = label['knowledge'][0]
+        domain = knowledge_snippet['domain']
+        entity_id = str(knowledge_snippet['entity_id'])
+        doc_id = str(knowledge_snippet['doc_id'])
+        document = self.knowledge_base[domain][entity_id]['docs'][doc_id]
+        question = document['title'].strip()
+        answer = document['body'].strip()
 
-        # if sep token is not used to divide dialog context and knowledge answer, the parameter tokenizer is not needed
-        # anymore
-        input = ' '.join([f"{self.special_tokens[utterance['speaker']]} {utterance['text']}" for utterance in dialog_context]
-                         + [self.special_tokens['Ka'], answer])
+        # Retrieve ground-truth response
+        response = label['response'].strip()
 
-        return input, target
+        # Create input as concatenation of utterances and knowledge document
+        input = ' '.join([f"{SPECIAL_TOKENS[utterance['speaker']]} {utterance['text']}" for utterance in dialog_context]
+                         + [SPECIAL_TOKENS['Ka'], answer])
+
+        return input, response
 
 
 
 if __name__ == '__main__':
-    from transformers import AutoTokenizer
-    tokenizer = AutoTokenizer.from_pretrained('roberta-base', padding_side='left')
-    m = MultiWOZDataset(tokenizer, 'data/val/logs.json', 'data/val/labels.json', 'data/knowledge.json')
+    m = MultiWOZDataset('data/val/logs.json', 'data/val/labels.json', 'data/knowledge.json')
 
     from torch.utils.data import DataLoader
     dl = DataLoader(m, batch_size=8)
+
+    print(m[0])
